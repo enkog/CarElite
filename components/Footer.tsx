660a3ebaca85215f747d45d854f26073402582f7
enkog <nkirukaogu@gmail.com>
2023-09-11 11:41:49 +0100

    Implement the footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src="/logo.svg"
+            alt="Car Elite logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+          <p className="text-base text-gray-700">
+            CarElite 2023
+            <br /> All rights reserved &copy;
+          </p>
+        </div>
+        <div className="footer__links">
+          {footerLinks.map(({ title, links }) => (
+            <div className="footer__link" key={title}>
+              <h3 className="font-bold">{title}</h3>
+              {links.map(({ title, url }) => (
+                <Link
+                  key={title}
+                  href={url}
+                  className="
+                text-gray-500"
+                >
+                  {title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2023 CarElite. All Rights Reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
